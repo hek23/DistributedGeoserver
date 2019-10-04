e402c6fdb20eee5ce2abc2a847e94bdfde91ef3a
hector <hector.fuentealba@usach.cl>
2019-10-04 14:05:23 -0300

    last progress

--- DistributedGeoserverAPI/controllers/geoprocess.py
+++ DistributedGeoserverAPI/controllers/geoprocess.py
@@ -53,21 +53,27 @@ def intersection():
     layersName = request.json['layers']
     if len(layersName)<1:
         return "Need Layers"
-    '''creator = "SELECT 'SELECT ' || array_to_string(ARRAY(SELECT 'o' || '.' || c.column_name \
-        FROM information_schema.columns As c \
-            WHERE table_name = '{0}' \
-            AND  c.column_name NOT IN('fid', 'the_geom')\
-    ), ',') || ' FROM {0} As o'"'''
-    '''# Get columns for shape. Doesn't need "the_geom" and "fid"'''
-
-    #Prepare input area
-    #areasToProcess = prepareArea(request.json['geometry'])
-
 
     columns_names = "SELECT c.column_name as cName FROM information_schema.columns as c WHERE table_name = '{0}' and column_name != 'the_geom' and column_name != 'fid';"
-    intersection = "(SELECT {2} ST_AsGeojson(ST_Transform(ST_Intersection(the_geom::geometry, {1}::geometry)," \
-                   "4326)) FROM {0} where ST_Intersects(the_geom::geometry, {1}::geometry))"
+    #intersection = "(SELECT {2} ST_AsGeojson(ST_Transform(ST_Intersection(the_geom::geometry, {1}::geometry)," \
+    #               "4326)) FROM {0} where ST_Intersects(the_geom::geometry, {1}::geometry))"
+
+    intersection = "(SELECT {2} ST_AsGeojson(ST_Intersection(the_geom::geometry, {1}::geometry))" \
+                   "FROM {0} where ST_Intersects(the_geom::geometry, {1}::geometry))"
+
     #Could be ST_WITHIN(the_geom::geometry, {1}::geometry)
+
+    ####################### MULTIPLE LAYERS
+    #
+    #cols = [columns_names.format(shape_name.split[1]) for shape_name in layersName]
+
+    
+
+
+
+    ##########################################
+
+
 
     # Generate queries
     attributes_layer = columns_names.format(layersName[0].split(".")[1])
@@ -75,6 +81,7 @@ def intersection():
     cursor = psqlConnector.getCursor()
     # Get attibutes
     cursor.execute(attributes_layer)
+    # Attributes from layer (names)
     information_layer = cursor.fetchall()
     layer = ""
     for attr in information_layer:
@@ -94,13 +101,11 @@ def intersection():
     attributes = []
     for row in result:
         polygons.append(json.loads(row[-1])) # polygon
-        #print(row[attri])
         attr = {}
         for attri in range(len(information_layer)):
             print(row[attri])
             attr.update({information_layer[attri][0]: row[attri]})# attr
         attributes.append(attr)
-    # {"polygons":[], "attributes":[]}
     if(len(result)==0): #No response
         return jsonify({"error":"No data suitable to query"}), 404
     else:
